72e67041aebc9aa418516699bd8dcee4298a4781
fangshuyun <fangshuyun@jd.com>
2018-06-13 13:42:42 +0800

    update spring.xml

--- src/main/java/com/jd/dao/UserDao.java
+++ src/main/java/com/jd/dao/UserDao.java
@@ -1,8 +1,9 @@
 package com.jd.dao;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.jd.domain.User;
 
-public interface UserDao {
-	
-
+public interface UserDao extends JpaRepository<User, Integer>{
+	//简单增删改查自带
 }
