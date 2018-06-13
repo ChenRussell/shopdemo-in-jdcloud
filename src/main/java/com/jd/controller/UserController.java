a4fcf446612e1a0c9d8e39eb23dabf57c0910e0a
zhonghanhui <zhonghanhui@jd.com>
2018-06-13 14:01:18 +0800

    add exceptoin

--- src/main/java/com/jd/controller/UserController.java
+++ src/main/java/com/jd/controller/UserController.java
@@ -1,5 +1,21 @@
 package com.jd.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.jd.exception.CustomException;
+
+@Controller
+@RequestMapping("/user")
 public class UserController {
+	
+	@RequestMapping("/test")
+	public void test(String name) {
+		if(name==null) {
+			throw new CustomException("E881","name can not be null!!!");
+		}else {
+			throw new RuntimeException("a runtime exception!!!");
+		}
+	}
 
 }
