0f8a3c441c305ba4665d2b8d857fb9a1b5f0a24a
fangshuyun <fangshuyun@jd.com>
2018-06-13 14:06:01 +0800

    update many

--- src/main/java/com/jd/dao/UserDao.java
+++ src/main/java/com/jd/dao/UserDao.java
@@ -1,9 +1,25 @@
 package com.jd.dao;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import com.jd.domain.User;
 
 public interface UserDao extends JpaRepository<User, Integer>{
 	//简单增删改查自带
+	
+	//修改密码
+	@Transactional
+	@Modifying
+	@Query("update User set password = ?1 where username = ?2")
+	public boolean jupdateUser(String password,String username);
+	
+	//判断用户是否存在
+	@Transactional
+	@Modifying
+	@Query("select username where username = ?1")
+	public String jisExist(String username);
 }
