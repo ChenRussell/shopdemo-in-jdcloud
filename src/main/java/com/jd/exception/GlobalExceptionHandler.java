61d0abc9ca373267350e1008bb2b0a636a98e91f
zhonghanhui <zhonghanhui@jd.com>
2018-06-14 13:00:38 +0800

    update exception

--- src/main/java/com/jd/exception/GlobalExceptionHandler.java
+++ src/main/java/com/jd/exception/GlobalExceptionHandler.java
@@ -20,7 +20,13 @@ public class GlobalExceptionHandler {
 	public ModelAndView allExceptionHandler(Exception exception) {
 		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.setViewName("forward:/error.jsp");
-		modelAndView.addObject("errMsg",exception.getMessage());
+		String errMsg = exception.getMessage()+"\n";
+		StackTraceElement[] stack= exception.getStackTrace();
+		for(StackTraceElement s:stack) {
+			errMsg += s+"\n";
+		}
+		System.out.println(errMsg);
+		modelAndView.addObject("errMsg",errMsg);
 		return modelAndView;
 	}		
 }
